3db46d0d41e352b0b1a873cc67f6320842b4c9f0
rorypearson26 <rorypearson2605@gmail.com>
2022-03-10 23:04:38 +0000

    Initial split but needs work.

--- pre_processing/read_in.py
+++ pre_processing/read_in.py
@@ -1,4 +1,5 @@
 """Module for reading in raw text and performing basic processing."""
+from lib2to3.pgen2.pgen import DFAState
 from pathlib import Path
 
 import pandas as pd
@@ -8,12 +9,29 @@ class RawData:
     def __init__(self, file_name):
         self.file_name = file_name
         self.file_path = Path(__file__).parents[1] / "raw_export" / self.file_name
-        self.df = self.get_raw_dataframe()
-        pass
+        self.raw_series = self.get_raw_series()
+        self.df = self.process_dataframe()
 
-    def get_raw_dataframe(self):
-        df = pd.read_csv(self.file_path)
+    def get_raw_series(self):
+        raw_lines = self.get_convo_as_list()
+        raw_series = pd.Series(raw_lines)
+        return raw_series
+
+    def process_dataframe(self):
+        df = pd.DataFrame()
+        df[["date", "other"]] = self.raw_series.str.split("-", n=1, expand=True)
+        df[["person", "message"]] = df["other"].str.split(":", n=1, expand=True)
+        df.drop("other", inplace=True, axis=1)
         return df
+
+    @staticmethod
+    def split_columns(raw):
+        
+
+    def get_convo_as_list(self):
+        with open(self.file_path, "r") as f:
+            lines = f.read().splitlines()
+        return lines
 
 
 if __name__ == "__main__":
