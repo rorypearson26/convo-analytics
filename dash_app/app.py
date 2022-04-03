dd1fbb07875e2e34c8b7f3f861c4872e5eacb82d
rorypearson26 <rorypearson2605@gmail.com>
2022-04-03 22:36:23 +0100

    Changes for debugger

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,6 +1,7 @@
 from dash_app.main_dash import app
 from dash_app.main_layout import create_main_layout
 
+
 if __name__ == "__main__":
     app.layout = create_main_layout()
-    app.run_server(debug=True)
+    app.run_server(debug=True, use_debugger=False, use_reloader=False)
