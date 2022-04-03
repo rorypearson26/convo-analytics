11d08b4977111da1bb9ab7494323bae2dcdac36a
rorypearson26 <rorypearson2605@gmail.com>
2022-04-03 22:36:45 +0100

    Using bootstrap stylesheets

--- dash_app/main_dash.py
+++ dash_app/main_dash.py
@@ -1,4 +1,5 @@
 """File to define app to avoid circular imports."""
 from dash import Dash
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
