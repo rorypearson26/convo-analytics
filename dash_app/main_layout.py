8b3a8d227ed95e8fb855392c0f7e1a0d73804f88
rorypearson26 <rorypearson2605@gmail.com>
2022-04-05 22:08:28 +0100

    Trying some bootstrap components

--- dash_app/main_layout.py
+++ dash_app/main_layout.py
@@ -1,4 +1,5 @@
 from dash import html, dcc
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 import numpy as np
@@ -12,10 +13,24 @@ from dash_app.layouts.file_upload import upload_html
 
 
 def create_main_layout():
+    # main_layout = html.Div(
+    #     [
+    #         html.Div(
+    #             dbc.Row(html.H1("WhatsApp Analysis")),
+    #             dbc.Row([dbc.Col(html.Span(upload_html), width=6)]),
+    #         ),
+    #     ]
+    # )
     main_layout = html.Div(
-        children=[
-            html.H1(children="WhatsApp Analysis"),
-            upload_html,
+        [
+            dbc.Row(dbc.Col(html.H1("WhatsApp Analysis"))),
+            dbc.Row(
+                [
+                    dbc.Col(html.Div(upload_html)),
+                    dbc.Col(html.Div("One of three columns")),
+                    dbc.Col(html.Div("One of three columns")),
+                ]
+            ),
         ]
     )
     return main_layout
