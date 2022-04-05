20608b69da53b7143cf62407c3d6e81222d720cd
rorypearson26 <rorypearson2605@gmail.com>
2022-04-05 22:08:07 +0100

    Added reloader again

--- dash_app/app.py
+++ dash_app/app.py
@@ -4,4 +4,4 @@ from dash_app.main_layout import create_main_layout
 
 if __name__ == "__main__":
     app.layout = create_main_layout()
-    app.run_server(debug=True, use_debugger=False, use_reloader=False)
+    app.run_server(debug=True)
